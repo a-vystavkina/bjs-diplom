c203b90d3ced265cbf7d2597d34f90e2be6cb6ed
a-vystavkina <54661749+a-vystavkina@users.noreply.github.com>
2019-12-09 12:09:02 +0300

    Update main.js
    Main.js fixed

--- static/main.js
+++ static/main.js
@@ -2,7 +2,7 @@
 
 class Profile {
 	constructor({username, name: {firstName, lastName}, password}) {
-      this.username = name;
+      this.username = username;
       this.name = {firstName, lastName};
       this.password = password;
 	};
